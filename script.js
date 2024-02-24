97dc354b07d33a9ae95341551494ccac19567d48
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-02-23 21:33:25 -0500

    create function to check matching passwords

--- script.js
+++ script.js
@@ -4,29 +4,56 @@ const confirmPassword = document.getElementById("confirmPassword");
 const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
-
-//global boolean
+//global variables
 let isValid = false;
+let isPasswordMatch = false;
 
 function validateForm() {
-    // using contraint API
-    isValid = form.checkVisibility();
-    // console.log(isValid);
-    // style message for an error
+  // using contraint API
+  isValid = form.checkVisibility();
+  // console.log(isValid);
+  // style message for an error
+  // check if isValid = false
+  if (!isValid) {
     message.textContent = "Please fill out all the fields before submitted.";
     message.style.color = "red";
     messageContainer.style.borderColor = "red";
-    
+    return;
+  }
+
+  // then check if password is match
+  if (createPassword.value === confirmPassword.value) {
+    isPasswordMatch = true;
+    createPassword.style.borderColor = "green";
+    confirmPassword.style.borderColor = "green";
+  } else {
+    // otherwise display false style to user
+    isPasswordMatch = false;
+    message.textContent =
+      "Sorry, your password did not match. Please try again!";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    createPassword.style.borderColor = "red";
+    confirmPassword.style.borderColor = "red";
+    return;
+  }
+
+  // if both isValid and isPasswordMatch is true then show's successfully registered! message.
+  if (isValid && isPasswordMatch) {
+    message.textContent = "Successfully registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
 }
 
 // handle submit button
 // create function processForm()
 function processForm(e) {
-    e.preventDefault();
-    // console.log("event happen here", e)
-    // validate form function
-    validateForm();
-    // store data
+  e.preventDefault();
+  // console.log("event happen here", e)
+  // validate form function
+  validateForm();
+  // store data
 }
 
-form.addEventListener("submit", processForm)
+form.addEventListener("submit", processForm);
